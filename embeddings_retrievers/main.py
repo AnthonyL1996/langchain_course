bbcbaab59990d9d06f45df8a474bda45796bde34
Anthony Lannoote <atl@melexis.com>
2024-02-06 13:41:26 +0100

    Add readme

--- embeddings_retrievers/main.py
+++ embeddings_retrievers/main.py
@@ -13,6 +13,8 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--task", default="return a list of numbers")
 parser.add_argument("--language", default="python")
 args = parser.parse_args()
+
+# Experiements trying to run local model with GPU support
 # /Program Files/CodeBlocks/MinGW/bin/gcc.exe
 # /Program Files/CodeBlocks/MinGW/bin/g++.exe
 
@@ -21,15 +23,17 @@ args = parser.parse_args()
 # E_CXX_COMPILER=C:/Development/CodeBlocks/MinGW/bin/g++.exe"
 # pip install llama-cpp-python  --upgrade --force-reinstall --no-cache-dir
 
-llm = LlamaCpp(
-    model_path="/Users/Anthony/Downloads/LLM Models/mistral-7b-instruct-v0.1.Q4_K_M.gguf",
-    n_gpu_layers=20,
-    n_batch=512,
-    n_ctx=2048,
-    f16_kv=True,
-    callback_manager=CallbackManager([StreamingStdOutCallbackHandler()]),
-    verbose=True,
-)
+# llm = LlamaCpp(
+#     model_path="/Users/Anthony/Downloads/LLM Models/mistral-7b-instruct-v0.1.Q4_K_M.gguf",
+#     n_gpu_layers=20,
+#     n_batch=512,
+#     n_ctx=2048,
+#     f16_kv=True,
+#     callback_manager=CallbackManager([StreamingStdOutCallbackHandler()]),
+#     verbose=True,
+# )
+
+llm = OpenAI()
 
 code_prompt = PromptTemplate(
     template="Write a very short {language} function that will {task}",
